76a7f6089e240d8383b13f719ccb77e5402f4ee2
alirezahajikhani <alirezahajikhani@gmail.com>
2018-09-26 20:50:49 +0330

    first try to git a project

---
+++ app.py
@@ -0,0 +1,20 @@
+
+
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+@app.route('/')
+def receive_message():
+        # get whatever message a user sent the bot
+        output = request.get_json()
+        
+        message = output['message']['text']
+
+        params = {'chat_id': output['message']['chat']['id'], 'text': "you have sent this : " + message }
+        response = requests.post("https://api.telegram.org/bot546273093:AAFwUs0V9bY72ocYZzFw_3feJB3adTorK3U/sendMessage"    , data=params)
+        return "Message Processed"
+if __name__ == '__main__':
+    app.run()
+
+
