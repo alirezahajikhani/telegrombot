3382202268f305e9f0bc7362eecad6d3866adff0
alirezahajikhani <alirezahajikhani@gmail.com>
2018-09-28 12:03:01 +0330

    h

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,5 @@
-import requests
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/')
-def receive_message():
-        # get whatever message a user sent the bot
-        output = request.get_json()
-        message = output['message']['text']
-        params = {'chat_id': output['message']['chat']['id'], 'text': "you have sent this : " + message }
-        response = requests.post("https://api.telegram.org/bot546273093:AAFwUs0V9bY72ocYZzFw_3feJB3adTorK3U/sendMessage"    , data=params)
-        return "Message Processed"
-if __name__ == '__main__':
-    app.run()
+
+from app import routes
