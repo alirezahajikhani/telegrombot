a66cfee25d19f80dc3b46ed55d029ce41e2489b8
alirezahajikhani <alirezahajikhani@gmail.com>
2018-09-29 18:51:58 +0330

    a

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', user=user , posts = posts)
