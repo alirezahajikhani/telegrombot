e774571e183a1ec07cd4cb3136bcfb92d6f75824
alirezahajikhani <alirezahajikhani@gmail.com>
2018-10-05 12:01:57 +0330

    f

--- app/__init__.py
+++ app/__init__.py
@@ -23,12 +23,6 @@ def getMessage():
     return "!", 200
 
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
-    return "!", 200
-
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
